a29e57dc1177527e339cdbc26f9009ef65d40643
Craig Trim <cmtrim@us.ibm.com>
2015-10-23 15:17:22 -0700

    terms to dto

---
+++ commons-dict-dto/src/main/java/org/swtk/common/dict/dto/wiktionary/TermInfluence.java
@@ -0,0 +1,60 @@
+package org.swtk.common.dict.dto.wiktionary;
+
+import org.swtk.common.dict.dto.wiktionary.adapter.TermInfluenceAdapter;
+
+import com.trimc.blogger.commons.utils.string.StringUtils;
+
+public class TermInfluence {
+
+	private String foreignTerm;
+
+	private String englishTerm;
+
+	private String foreignLanguage;
+
+	public String getEnglishTerm() {
+		return englishTerm;
+	}
+
+	public String getForeignLanguage() {
+		return foreignLanguage;
+	}
+
+	public String getForeignTerm() {
+		return foreignTerm;
+	}
+
+	public boolean hasEnglishTerm() {
+		return StringUtils.hasValue(getEnglishTerm());
+	}
+
+	public boolean hasForeignLanguage() {
+		return StringUtils.hasValue(getForeignLanguage());
+	}
+
+	public boolean hasForeignTerm() {
+		return StringUtils.hasValue(getForeignTerm());
+	}
+
+	@Override
+	public int hashCode() {
+		return this.toString().hashCode();
+	}
+
+	public void setEnglishTerm(String englishTerm) {
+		this.englishTerm = englishTerm;
+	}
+
+	public void setForeignLanguage(String foreignLanguage) {
+		this.foreignLanguage = foreignLanguage;
+	}
+
+	public void setForeignTerm(String foreignTerm) {
+		this.foreignTerm = foreignTerm;
+	}
+
+	@Override
+	public String toString() {
+		return TermInfluenceAdapter.toString(this);
+	}
+}
---
+++ commons-dict-dto/src/main/java/org/swtk/common/dict/dto/wiktionary/adapter/TermInfluenceAdapter.java
@@ -0,0 +1,33 @@
+package org.swtk.common.dict.dto.wiktionary.adapter;
+
+import org.swtk.common.dict.dto.utils.WikiCodeLangLookup;
+import org.swtk.common.dict.dto.wiktionary.TermInfluence;
+
+import com.trimc.blogger.commons.exception.AdapterValidationException;
+import com.trimc.blogger.commons.utils.string.StringUtils;
+
+public final class TermInfluenceAdapter {
+
+	public static String toString(TermInfluence bean) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(bean.getEnglishTerm());
+		sb.append(", foreign-term = " + bean.getForeignTerm());
+		sb.append(", foreign-language = " + bean.getForeignLanguage());
+
+		return sb.toString();
+	}
+
+	public static TermInfluence tranform(String foreignTerm, String englishTerm, String foreignLanguageCode) throws AdapterValidationException {
+		TermInfluence bean = new TermInfluence();
+
+		String foreignLanguage = WikiCodeLangLookup.getLangauge(foreignLanguageCode);
+		if (!StringUtils.hasValue(foreignLanguage)) throw new AdapterValidationException("Unrecognized Langauge Code (value = %s)", foreignLanguageCode);
+
+		bean.setEnglishTerm(englishTerm);
+		bean.setForeignTerm(foreignTerm);
+		bean.setForeignLanguage(foreignLanguage);
+
+		return bean;
+	}
+}
